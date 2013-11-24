429216a7207d23fe9c304d258c95f47f307cde7a
Kai Xiong <xkeecs@gmail.com>
2013-11-23 20:00:06 -0500

    tab activity added

---
+++ src/com/photolocator/tab/NearByFeedsActivity.java
@@ -0,0 +1,38 @@
+package com.photolocator.tab;
+
+import android.os.Bundle;
+
+import com.photolocator.R;
+
+public class NearByFeedsActivity extends TabItemActivity
+{
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_nearbyfeeds);
+		System.out.println("NearByFeedsActivity");
+	}
+
+	@Override
+	protected void initViews()
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected void initEvents()
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected void init()
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
